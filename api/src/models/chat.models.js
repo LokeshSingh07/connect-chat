a1c84415551f7807155feee095f6c8ea4da2fc5f
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-07-12 16:22:52 +0530

    user, chat, message models
    middleware for encrypting password before saving the doc
    
    matchPassword & generateAccessToken methods created

---
+++ api/src/models/chat.models.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const chatSchema = new Schema({
+    chatName: {
+        type: String,
+        trim:true,
+    },
+    isGroupChat:{
+        type: Boolean,
+        default:false,
+    },
+    users:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    latestMessage:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message',
+    },
+    groupAdmin:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+
+
+
+}, {timestamps : true})
+
+module.exports = mongoose.model("Chat", chatSchema);
